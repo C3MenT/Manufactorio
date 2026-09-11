5125fcbd4caed62e6e7e54a6ea65619ff651e49c
Ri'chard, "Ric," "Dick," "Richard," etc. <107008413+C3MenT@users.noreply.github.com>
2026-09-11 14:48:45 -0700

    Added a global inventory relation and a tiny game state relation. Also added a couple of basic triggers to start facilitating a "game loop"

--- init.sql
+++ init.sql
@@ -44,41 +44,46 @@ CREATE TABLE if NOT EXISTS Extractors (
     biome_id int NOT NULL REFERENCES Biomes(id), -- Foreign key to Biomes table (for biome extractor is in)
     resource_id int NOT NULL REFERENCES Resources(id), -- Foreign key to Resources table (for resource extractor is extracting)
     type varchar(30) NOT NULL, -- Type of extractor (e.g., mining rig, water pump, etc.)
-    efficiency int NOT NULL, -- Efficiency value used in resource yield calculations
-    amount_stored float NOT NULL -- Amount of resource currently stored in the extractor
+    efficiency float NOT NULL DEFAULT 1.0, -- Efficiency value used in resource yield calculations
+    amount_stored float NOT NULL DEFAULT 0 -- Amount of resource currently stored in the extractor
 );
 
 CREATE TABLE if NOT EXISTS Fabricators (
     id int PRIMARY KEY,
     type varchar(30) NOT NULL, -- Type of fabricator (e.g., smelter, assembler, etc.)
-    efficiency int NOT NULL, -- Efficiency value used in resource yield calculations
+    efficiency float NOT NULL DEFAULT 1.0, -- Efficiency value used in resource yield calculations
     amount_stored float NOT NULL DEFAULT 0, -- Amount of material or products currently stored in the fabricator (products will be natural numbers)
     product varchar(30) NOT NULL, -- Name of material or product being produced by the fabricator
     byproduct varchar(30) -- Name of byproduct being produced by the fabricator (can be NULL if no byproduct is produced)
 
 );
 
--- JUNCTION TABLES 
+-- JUNCTION TABLES
+
+-- Resources:Biomes (Many-to-Many)
 CREATE TABLE if NOT EXISTS Resource_Locations (
     biome_id int NOT NULL REFERENCES Biomes(id), -- Foreign key to Biomes table (for biome resource is in)
     resource_id int NOT NULL REFERENCES Resources(id), -- Foreign key to Resources table (for resource in biome)
     PRIMARY KEY (biome_id, resource_id) -- Composite primary key to ensure uniqueness of biome-resource pairs
 );
 
+-- Fabricators:Biomes (Many-to-Many)
 CREATE TABLE if NOT EXISTS Fabricator_Locations (
     biome_id int NOT NULL REFERENCES Biomes(id), -- Foreign key to Biomes table (for biome fabricator is in)
     fabricator_id int NOT NULL REFERENCES Fabricators(id), -- Foreign key to Fabricators table (for fabricator in biome)
     PRIMARY KEY (biome_id, fabricator_id) -- Composite primary key to ensure uniqueness of biome-fabricator pairs
 );
 
+-- Extractors:Biomes (Many-to-Many)
 CREATE TABLE if NOT EXISTS Extractor_Locations (
     biome_id int NOT NULL REFERENCES Biomes(id), -- Foreign key to Biomes table (for biome extractor is in)
     extractor_id int NOT NULL REFERENCES Extractors(id), -- Foreign key to Extractors table (for extractor in biome)
     PRIMARY KEY (biome_id, extractor_id) -- Composite primary key to ensure uniqueness of biome-extractor pairs
 );
 
+-- Resources/Materials:Products (and Byproducts) (Many-to-Many)
 CREATE TABLE if NOT EXISTS Recipes (
-    name varchar(30) NOT NULL PRIMARY KEY,
+    name varchar(30) PRIMARY KEY,
     type varchar(30) NOT NULL, -- Type of product being produced by the recipe (e.g., material, product, etc.)
     fabricator_type varchar(30) NOT NULL, -- Type of fabricator (e.g., smelter, assembler, etc.)
     input_material varchar(30) NOT NULL, -- An input material required for the output
@@ -86,3 +91,81 @@ CREATE TABLE if NOT EXISTS Recipes (
     byproduct varchar(30), -- A byproduct produced by the recipe (can be NULL if no byproducts are produced)
     UNIQUE (input_material, product, byproduct) -- Ensure uniqueness of recipe combinations
 );
+
+
+CREATE TABLE if NOT EXISTS Inventory (
+    item_name varchar(30) PRIMARY KEY,
+    amount_stored float NOT NULL DEFAULT 0 CHECK (amount_stored >= 0),-- Amount of item currently stored in total (across all fabricators and extractors)
+    amount_per_cycle float NOT NULL DEFAULT 0 -- Amount of item produced or consumed per cycle (can be negative for dominant consumption)
+);
+
+
+CREATE TABLE if NOT EXISTS Global_State (
+    id int PRIMARY KEY CHECK (id = 1), -- Ensure only one row exists in the table
+    cycle int NOT NULL DEFAULT 0 -- Current cycle number, starting from 0
+);
+
+
+-- TRIGGERS
+
+
+-- Core Driver Trigger, used for the game loop
+-- A frame is a single update of the cycle attribute
+CREATE TRIGGER IF NOT EXISTS new_cycle
+    AFTER UPDATE OF cycle ON Global_State
+    BEGIN
+        
+        -- Update Extractors
+        UPDATE Extractors
+        SET amount_stored = amount_stored + (
+            efficiency * (
+                SELECT abundance 
+                FROM Resources 
+                WHERE Resources.id = Extractors.resource_id) * (
+                    SELECT purity 
+                    FROM Resources 
+                    WHERE Resources.id = Extractors.resource_id)
+            );
+        
+        -- Update Fabricators
+        UPDATE Fabricators
+        SET amount_stored = amount_stored + (
+            efficiency * (
+                SELECT abundance 
+                FROM Resources 
+                WHERE Resources.id = Fabricators.resource_id) * (
+                    SELECT purity 
+                    FROM Resources 
+                    WHERE Resources.id = Fabricators.resource_id)
+            );
+        
+        
+
+        -- Update global inventory
+        UPDATE Inventory
+        -- Sum of all amounts produced and consumed per cycle for each item in the inventory
+        -- Or specifically,
+        -- Term 1: Sum of all amounts produced by extractors for each resource in the inventory
+        SET amount_per_cycle = (
+            SELECT SUM(amount_stored) 
+            FROM Extractors 
+            WHERE Extractors.resource_id = (
+                SELECT id 
+                FROM Resources 
+                WHERE Resources.name = Inventory.item_name)
+        -- Term 2: Sum of all amounts produced by fabricators for each product in the inventory
+        ) + (
+            SELECT SUM(amount_stored) 
+            FROM Fabricators 
+            WHERE Fabricators.product = Inventory.item_name
+        -- Term 3: Sum of all amounts consumed by recipes for each input material in the inventory
+        ) - (
+            SELECT SUM(amount_stored) 
+            FROM Recipes 
+            WHERE Recipes.input_material = Inventory.item_name
+        );
+        SET amount_stored = amount_stored + amount_per_cycle
+        WHERE item_name IN (SELECT item_name FROM Inventory);
+
+    END;
+
